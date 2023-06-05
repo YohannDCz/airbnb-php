f1066e5d6fabdfe200dca3ce99981ac643370b73
Atigou-Bah <atigoubah2003@gmail.com>
2023-06-05 14:56:57 +0200

    First_commit

---
+++ contactus.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Contactez-nous</h1>
+    <div class="box_1">
+    <form action="">
+     <input type="text" name="" id="" placeholder="Prénom">
+     <input type="text" name="" id="" placeholder="Nom">
+     <input type="mail" name="" id="" placeholder="Adresse mail">
+     <input type="tel" name="" id="tel" placeholder="Téléphone portable">
+    </form>
+    </div>
+    <div class="box_2">
+        <form action="" method="post">
+            <input type="text" placeholder="Objet">
+            <input type="text" placeholder="Message">
+        </form>
+    </div class="Contact">
+   <div>
+    <div>
+        <button> Envoyer </button>
+    </div>
+
+    <div>
+        06.01.02.03.04 <br>
+        tailledor@live.fr
+    </div>
+    </div>
+</body>
+</html>
