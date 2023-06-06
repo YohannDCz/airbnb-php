f7e50d616a52f715e1d2c384ab6af8a6e09c62fb
Yohann DC <YohannDCz@gmail.com>
2023-06-06 08:54:03 +0200

    Routeur Partie 1

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<?php
+ini_set('display_errors', 1);
+// inclure les routers nécessaires
+
+// récupérer la méthode et l'URL de la requête
+$method = $_SERVER['REQUEST_METHOD'];
+$url = $_SERVER['REQUEST_URI'];
+
+//  host
+$host = 'localhost:3000';
+
+// sépare l'url en plusieurs sections pour le routage
+$split_url = explode('/', $url);
+
+//  démarrage session
+session_start();
+
+//Router
+switch($split_url[1]){
+    // Route utilisateur de l'API
+    case 'user':
+        // require le router user
+        require_once './src/router/userRoutes.php';
+        break;
+    case 'pages':
+        //  require le router page
+        require_once './src/router/pagesRoutes.php';
+        break;
+    case 'gestion':
+        //  require le router group
+        require_once './src/router/gestionRoutes.php';
+        break;
+    case 'rating':
+        //  require le router post
+        require_once './src/router/ratingRoutes.php';
+        break;
+    case 'functions':
+        //  require le routeur des fonctions
+        require_once './src/router/functionsRoutes.php';
+        break;
+    case 'home' :
+        //require la page homepage
+        require_once './template/user/homepage.php';
+        break;
+    default :
+        //require la page d'erreur 404
+        require_once './template/404.php';
+        break;
+}
+
+?>
