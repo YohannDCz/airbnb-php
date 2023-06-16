621143fa6681deeeb7b991e7248945f9acb06458
Atigou-Bah <atigoubah2003@gmail.com>
2023-06-16 10:58:31 +0200

    Reorganisation des fichiers

--- src/Page PHP/login.php
+++ src/Page PHP/login.php
@@ -5,10 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./src/style/style.css">
-    <link rel="stylesheet" href="./src/style/login.css">
-    <script src="./src/javascript/header.js" defer></script>
-
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
 <header >
