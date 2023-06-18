a30f571df5dddc893f89f539235df31d40892555
Yohann DC <YohannDCz@gmail.com>
2023-06-18 20:04:00 +0200

    Code review login

--- templates/user/login.php
+++ templates/user/login.php
@@ -1,68 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="login.css">
-</head>
-<body>
-<header >
-        <div class="left">
-        <div class="logo"><img  src="./assets/img/logo.png"  class="img_logo" alt=""> <h3>TAILLE D'OR</h3></div>
-    
-          <nav class="menu">
-            <ul>
-              <li><a href="#">Account</a></li>
-              <li><a href="#">Voyages</a></li>
-              <li><a href="#">Favoris</a></li>
-              <li><a href="#">Signout</a></li>
-            </ul>
-            <div class="close-btn">&times;</div>
-          </nav>
-          <div class="nav_links">
-            <a href="">Accueil</a>
-            <a href="">Locations</a>
-            <a href="">Experiences</a>
-            <a href="">Dashboard</a>
-          </div>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="../css/login.css">
+    </head>
+    <body>
+        <?php include "../components/header-mini.php" ?>
+        <div class="login">
+            <h1>Connexion</h1>
+            <form action>
+                <div class="form">
+                <div class="box_login">
+                    <input type="email" name id placeholder="Adresse mail @">
+                    <input type="password" name id="mot_de_passe"
+                        placeholder="Mot de passe">
+                </div>
+                <div class="link_password">
+                    <a href="#" id="left">Mot de passe oublié ?</a> 
+                    <a href="#" id="right">S'inscrire</a>
+                </div>
+                <div class="btn">
+                    <button> Continuer <img
+                    src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-right-c-1024.png"
+                    alt="flèche"> </button>
+                </div>
+                </div>
+            </form>
         </div>
-
-          <div class="right">
-          <div class="icons">
-            <a href="lien_image1.html"><img src="./assets/img/Fr.svg" alt="fr"></a>
-            <a href="lien_image2.html"><img src="./assets/img/search.svg" alt="LP"></a>
-         </div>
-
-            <div class="menu-burger">
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
-            </div>
-
-        <div class="icons2">
-                <a href="lien_image4.html"><img src="./assets/img/profil.png" alt="rl"></a>
-        </div>
-       </div>
-
-        
-    </header>
-    <div class="login"> 
-    <h1>Connexion</h1>
-    <form action="">
-    <div class="box_login">
-        <input type="email" name="" id="" placeholder="Adresse mail @">
-        <input type="password" name="" id="mot_de_passe" placeholder="Mot de passe">
-    </div>
-    <div class="link_password">
-    <a href="#" id="left">Mot de passe oublié ?</a> <a href="#" id="right">S'inscrire</a>
-</div>
-</form>
-<div class="btn">
-    <button> Continuer <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-right-c-1024.png" alt="flèche"> </button>
-</div>
-</div>
-</body>
+        <?php include "../components/footer.php" ?>
+    </body>
 </html>
