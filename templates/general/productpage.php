21b58bf061b4da70a3dc7952f3bf7a9e944d3aaa
Adrien F <adrien.formoso@live.fr>
2023-06-20 16:05:01 +0200

    fix button review review homepage

---
+++ templates/general/productpage.php
@@ -0,0 +1,177 @@
+<?php
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        <?php
+        include("../css/productpage/product.css");
+        include("../css/productpage/disponibilite.css");
+        include("../css/productpage/carouselle.css");
+        include("../css/productpage/review.css");
+        include("../css/productpage/productpage.css");
+        ?>
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <header></header>
+    <div id="product">
+        <div id="aera_product">
+            <div id="left_aera_product">
+                <div id="main_img">
+                    <img src="../../assets/img/img_facade_villa.jpg" alt="">
+                </div>
+                <div id="other_img">
+                    <img src="../../assets/img/argument1.png" alt="">
+                    <img src="../../assets/img/argument1.png" alt="">
+                    <img src="../../assets/img/argument1.png" alt="">
+                </div>
+            </div>
+            <div id="right_aera_product">
+                <h1></h1>
+                <div id="adresse">
+                    <img src="" alt="">
+                    <p></p>
+                </div>
+                <div id="price">
+                    <span></span>
+                    <p></p>
+                </div>
+                <div id="calender" >
+                    <div id="calender_du">
+                        <p></p>
+                        <input type="date" name="" id="">
+                    </div>
+                    <div id="calender_au">
+                        <p></p>
+                        <input type="date" name="" id="">
+                    </div>
+                    <p>Séjour de <span></span></p>
+                </div>
+                <div id="total">
+
+                </div>
+                <div id="garanties">
+
+                </div>
+                <div id="submit_div">
+                    <button>
+
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="dispomibilite">
+    <h3>Retour d'esperience</h3>
+        <div id="area_dispomibilite_slider">
+            <div id="dispomibilite_slider">
+                <div class="dispomibilite_element">
+                    <h3>Sophie Dupont</h3>
+                    <p>Superbe logement ! Propre, spacieux et décoré avec goût. Nous avons passé un séjour vraiment agréable.</p>
+                </div>
+                <div class="dispomibilite_element">
+                    <h3>Sophie Dupont</h3>
+                    <p>Superbe logement ! Propre, spacieux et décoré avec goût. Nous avons passé un séjour vraiment agréable.</p>
+                </div>
+                <div class="dispomibilite_element">
+                    <h3>Sophie Dupont</h3>
+                    <p>Superbe logement ! Propre, spacieux et décoré avec goût. Nous avons passé un séjour vraiment agréable.</p>
+                </div>
+                <div class="dispomibilite_element">
+                    <h3>Sophie Dupont</h3>
+                    <p>Superbe logement ! Propre, spacieux et décoré avec goût. Nous avons passé un séjour vraiment agréable.</p>
+                </div>
+                <div class="dispomibilite_element">
+                    <h3>Sophie Dupont</h3>
+                    <p>Superbe logement ! Propre, spacieux et décoré avec goût. Nous avons passé un séjour vraiment agréable.</p>
+                </div>
+                <div class="dispomibilite_element">
+                    <h3>Sophie Dupont</h3>
+                    <p>Superbe logement ! Propre, spacieux et décoré avec goût. Nous avons passé un séjour vraiment agréable.</p>
+                </div>
+                <div class="dispomibilite_element">
+                    <h3>Sophie Dupont</h3>
+                    <p>Superbe logement ! Propre, spacieux et décoré avec goût. Nous avons passé un séjour vraiment agréable.</p>
+                </div>
+                <div class="dispomibilite_element">
+                    <h3>Sophie Dupont</h3>
+                    <p>Superbe logement ! Propre, spacieux et décoré avec goût. Nous avons passé un séjour vraiment agréable.</p>
+                </div>
+                <div class="dispomibilite_element">
+                    <h3>Sophie Dupont</h3>
+                    <p>Superbe logement ! Propre, spacieux et décoré avec goût. Nous avons passé un séjour vraiment agréable.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="carouselle">
+
+    </div>
+    <div id="review">
+    <h3>Retour d'esperience</h3>
+        <div id="area_review">
+            <div id="left_review_btn" >
+                <button>
+                    <img src="../../assets/logo/arrow-black.png" alt="">
+                </button>
+            </div>
+            <div id="area_review_slider">
+                <div id="review_slider">
+                    <div class="review_element">
+                        <h3>Sophie Dupont</h3>
+                        <p>Superbe logement ! Propre, spacieux et décoré avec goût. Nous avons passé un séjour vraiment agréable.</p>
+                    </div>
+                    <div class="review_element">
+                        <h3>Alexandre Martin</h3>
+                        <p>L'emplacement de ce logement est idéal. Il est proche des attractions touristiques, des restaurants et des transports en commun. Parfait pour explorer la ville.</p>
+                    </div>
+                    <div class="review_element">
+                        <h3>Emma Tremblay</h3>
+                        <p>Les propriétaires sont très accueillants et attentifs à nos besoins. Ils nous ont fourni toutes les informations nécessaires pour profiter au maximum de notre séjour.</p>
+                    </div>
+                    <div class="review_element">
+                        <h3>Gabriel Sanchez</h3>
+                        <p>Les propriétaires sont très accueillants et attentifs à nos besoins. Ils nous ont fourni toutes les informations nécessaires pour profiter au maximum de notre séjour.</p>
+                    </div>
+                    <div class="review_element">
+                        <h3>Olivia Thompson</h3>
+                        <p>Les propriétaires sont très accueillants et attentifs à nos besoins. Ils nous ont fourni toutes les informations nécessaires pour profiter au maximum de notre séjour.</p>
+                    </div>
+                    <div class="review_element">
+                        <h3>Liam Patel</h3>
+                        <p>La résidence est très calme et sécurisée. Nous nous sommes sentis en toute tranquillité pendant notre séjour, ce qui a ajouté une dimension de relaxation supplémentaire.</p>
+                    </div>
+                    <div class="review_element">
+                        <h3>Charlotte Dubois</h3>
+                        <p>La résidence est très calme et sécurisée. Nous nous sommes sentis en toute tranquillité pendant notre séjour, ce qui a ajouté une dimension de relaxation supplémentaire.</p>
+                    </div>
+                    <div class="review_element">
+                        <h3>Noah Kim</h3>
+                        <p>La résidence est très calme et sécurisée. Nous nous sommes sentis en toute tranquillité pendant notre séjour, ce qui a ajouté une dimension de relaxation supplémentaire.</p>
+                    </div>
+                    <div class="review_element">
+                        <h3>Mia Garcia</h3>
+                        <p>Le lit était extrêmement confortable, ce qui nous a permis de bien nous reposer après des journées bien remplies de visites et d'activités.</p>
+                    </div>
+                    <div class="review_element">
+                        <h3>Lucas Anderson</h3>
+                        <p>Nous recommandons vivement ce logement à tous ceux qui souhaitent passer un séjour mémorable. Nous n'hésiterons pas à réserver à nouveau lors de notre prochaine visite.</p>
+                    </div>
+                </div>
+            </div>
+            <div id="right_review_btn">
+                <button>
+                    <img src="../../assets/logo/arrow-black.png" alt="">
+                </button>
+            </div>
+        </div>
+    </div>
+    <footer>
+
+    </footer>
+    <script src="../script/productpage.js"></script>
+</body>
+</html>
