f115152e03597729271281c759425cca95c17ea9
YohannDCz <36419948+YohannDCz@users.noreply.github.com>
2023-06-23 10:00:54 +0200

    Update profil_reservation.php

--- templates/user/profil_reservation.php
+++ templates/user/profil_reservation.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/profil/styles_profil_reservation.css">
+    <link rel="stylesheet" href="../styles/profile/styles_profil_reservation.css">
     <title>Document</title>
 </head>
 <body>
